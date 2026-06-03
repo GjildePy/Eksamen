f6324af83428a055efbe5848c60333cdea752a27
Gjilde <gildeoliver07@gmail.com>
2026-06-03 10:13:10 +0200

    rapport system funksjon
    script.js funksjon for knappen

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function sendRapport(event) {
 
-    fetch ('/rapporter' {
+    const tekst = document.getElementById('problemTekst').value;
+
+    fetch ('/rapporter', {
         method: 'POST',
-        headers: '',
-        body: 
-    })
+        headers: {'Content-type': 'application/x-www-form-urlencoded' },
+        body: 'problemMelding=' + encodeURIComponent(tekst)
+    });
+
+    alert("Takk! Rapporten din er mottatt")
+
 }
